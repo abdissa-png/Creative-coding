62ccf7e91571737acfca58b63f824ab43acec621
abdissa-png <116519055+abdissa-png@users.noreply.github.com>
2022-10-26 09:46:58 +0300

    Add files via upload

---
+++ Mandelbrot .py
@@ -0,0 +1,27 @@
+import math
+import turtle
+a=turtle.Turtle()
+a.speed(0)
+a.pencolor("red")
+def draw():
+	for i in range(-200,201,1):
+		for j in range(-200,201,1):
+			if (mandelbrot([0,0],[i/100,j/100],0)):
+				a.penup()
+				a.goto([i,j])
+				a.pendown()
+				a.circle(2)
+def mandelbrot(arr,offset,count):
+	if (math.sqrt((arr[0]**2)+(arr[1]**2))<=2) and count>100:
+		return True
+	elif math.sqrt((arr[0]**2)+(arr[1]**2))>2:
+		return False
+	else:
+		x=arr[0]**2-arr[1]**2+offset[0]
+		y=2*arr[0]*arr[1]+offset[1]
+		arr=[x,y]
+		count+=1
+		return mandelbrot(arr,offset,count)
+turtle.mainloop()
+draw()
+turtle.done()
